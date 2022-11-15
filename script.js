1f1b3f3a2ee16057562b32b7813b8885bb953a46
Mateus Schulle <89845962+mateusschulle@users.noreply.github.com>
2022-11-15 20:49:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,28 @@ function exibeMenu() {
     if (verificador) {
         document.querySelector('.navegacao__lista').style.cssText ='display: flex;';
         verificador = false;
+        document.querySelector('.container__login').style.cssText ='display: none;';
+        verificadorLogin = true;
     } else {
         document.querySelector('.navegacao__lista').style.cssText ='display: none;';
         verificador = true;
+    }
+}
+
+let botaoUsuario = document.querySelector('.botao__usuario');
+botaoUsuario.onclick = exibeLogin;
+
+let verificadorLogin = true;
+
+function exibeLogin() {
+    if (verificadorLogin) {
+        document.querySelector('.container__login').style.cssText ='display: flex;';
+        verificadorLogin = false;
+        document.querySelector('.navegacao__lista').style.cssText ='display: none;';
+        verificador = true;
+    } else {
+        document.querySelector('.container__login').style.cssText ='display: none;';
+        verificadorLogin = true;
+        
     }
 }
